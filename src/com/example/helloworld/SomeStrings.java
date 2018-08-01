fdfb065287fbbfb4891d9b5b94e0287d97136108
Gabriel A Pass <gabriel@gapmedia.org>
2018-08-01 14:44:11 -0500

    Added example helloworld

---
+++ src/com/example/helloworld/SomeStrings.java
@@ -0,0 +1,55 @@
+package com.example.helloworld;
+
+public class SomeStrings {
+    public static void main(String[] args) {
+        String message1 = "This is a test";
+        String message2 = "Hello Class";
+
+        System.out.println(message1.length());
+        System.out.println(message2.length());
+
+        System.out.println(message1.substring(0,3));
+        System.out.println(message1.substring(5));
+
+        System.out.println(message2.indexOf("Hello"));
+
+        /* Part 2 */
+        System.out.println("-----SECTION 2-----");
+        System.out.println(message1.compareTo("This is a te"));
+
+        /* part 3 */
+        System.out.println("-----SECTION 3-----");
+        String s1 = "Hello";
+        String s2 = "Hello";
+        System.out.println(s1 == s2);
+        System.out.println(s1.equals(s2));
+
+        /* part 4 */
+        System.out.println("------SECTION 4-----");
+        String st1 = "Happy Birthday";
+        String st2 = "Jose";
+        String result = st1 + " " + st2;
+        System.out.println(result);
+
+
+        /* part 5 */
+        System.out.println("-----SECTION 5-----");
+        String message3 = "12" + 4 + 3;
+        System.out.println(message3);
+
+        String message4 = "12" + (4 + 3);
+        System.out.println(message4);
+
+        message4 = "All Done!";
+        System.out.println(message4);
+
+        /* part 6 */
+        System.out.println("-----SECTION 6-----");
+        boolean isOpen = true;
+        boolean gotMoney = false;
+        boolean CantRefuse = false;
+        if ((isOpen && gotMoney) || CantRefuse) System.out.println("Wanna play a game?");
+        else System.out.println("We're Closed! Go Home!");
+        System.out.println("all done...");
+    }
+}
